cbaed20c0669124594c511d101330efa1f7e998d
Chloe Nan Huang <72413057+chloehuang123@users.noreply.github.com>
2022-09-04 09:50:09 -0400

    Create 238_Product_of_Array_Except_Self.py

---
+++ Leetcode/238_Product_of_Array_Except_Self.py
@@ -0,0 +1,17 @@
+###############################################################################################
+# 思路：乘积 = 当前数左边的乘积 * 当前数右边的乘积
+###############################################################################################
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
