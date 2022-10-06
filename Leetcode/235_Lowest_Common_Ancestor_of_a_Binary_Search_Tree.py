3cb6d6143634b6cea12f12c48e3a4443b1085641
Chloe Nan Huang <72413057+chloehuang123@users.noreply.github.com>
2022-10-06 09:24:37 -0400

    Create 235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py

---
+++ Leetcode/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        curr = root
+
+        while curr:
+            if curr.val > p.val and curr.val > q.val:
+                curr = curr.left
+            elif curr.val < p.val and curr.val < q.val:
+                curr = curr.right
+            else:
+                return curr
+
+# 注意大于小于号，主要看p.val, q.val是在左边还是右边
