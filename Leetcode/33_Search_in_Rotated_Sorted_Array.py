539c82f4a34593a8891c93d84fb3a66043e1d059
Chloe Nan Huang, PhD <72413057+chloehuang123@users.noreply.github.com>
2023-08-10 19:39:41 -0400

    Update 33_Search_in_Rotated_Sorted_Array.py

--- Leetcode/33_Search_in_Rotated_Sorted_Array.py
+++ Leetcode/33_Search_in_Rotated_Sorted_Array.py
@@ -1,3 +1,11 @@
+"""
+如果 [l, mid - 1] 是有序数组，且 target 的大小满足 [nums[l],nums[mid])[\textit{nums}[l],\textit{nums}[mid])[nums[l],nums[mid])，则我们应该将搜索范围缩小至 [l, mid - 1]，否则在 [mid + 1, r] 中寻找。
+如果 [mid, r] 是有序数组，且 target 的大小满足 (nums[mid+1],nums[r]](\textit{nums}[mid+1],\textit{nums}[r]](nums[mid+1],nums[r]]，则我们应该将搜索范围缩小至 [mid + 1, r]，否则在 [l, mid - 1] 中寻找。
+
+"""
+
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
