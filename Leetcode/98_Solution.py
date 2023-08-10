48b7b212fa674e361879b4c8f776e5818c274713
Chloe Nan Huang, PhD <72413057+chloehuang123@users.noreply.github.com>
2023-08-10 15:27:47 -0400

    Create 98_Solution.py

---
+++ Leetcode/98_Solution.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        self.lst = []
+        self.dfs(root)
+        
+        for i in range(len(self.lst)-1):
+            if self.lst[i+1] >= self.lst[i]:
+                return True
+            else:
+                return False
+        
+    
+    
+    def dfs(self, root):
+        if not root:
+            return
+        
+        self.dfs(root.left)
+        self.lst.append(root.val)
+        self.dfs(root.right)
