03e48cf6ec8d7618acacf5511f139cd4c1d265a9
Chloe Nan Huang, PhD <72413057+chloehuang123@users.noreply.github.com>
2023-08-13 10:26:10 -0400

    Create 215_Solution.py

---
+++ Leetcode/215_Solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        sorted_nums = self.merge_sort(nums)
+        return sorted_nums[-k]
+
+    def merge_sort(self, nums):
+        if len(nums) <= 1:
+            return nums
+        
+        mid = len(nums) // 2
+        left = self.merge_sort(nums[:mid])
+        right = self.merge_sort(nums[mid:])
+        return self.merge(left, right)
+        
+
+
+    def merge(self,left, right):
+        low, high = 0,0
+        res = []
+
+        while low < len(left) and high < len(right):
+            if left[low] < right[high]:
+                res.append(left[low])
+                low += 1
+            else:
+                res.append(right[high])
+                high += 1
+            
+        res.extend( left[low:] )
+        res.extend( right[high:] )
+        return res
