756b3cc7e086704d532a3f05f2fb464aee8e5fb7
Chloe Nan Huang, PhD <72413057+chloehuang123@users.noreply.github.com>
2023-08-14 11:32:55 -0400

    Update 80_Solution.py

--- Leetcode/80_Solution.py
+++ Leetcode/80_Solution.py
@@ -1,3 +1,9 @@
+"""
+each unique element appears at most twice
+Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3,_]
+"""
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         i = 2
