3979c2798ae04ce7677758be2164d1cfc7458b15
Chloe Nan Huang, PhD <72413057+chloehuang123@users.noreply.github.com>
2023-08-14 13:30:09 -0400

    Create 75_Solution.py

---
+++ Leetcode/75_Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        curr, left, right = 0,0,len(nums) - 1
+
+        while curr <= right:
+            if nums[curr] == 0:
+                nums[left], nums[curr] = nums[curr], nums[left]
+                left += 1
+                curr += 1
+            elif nums[curr] == 2:
+                nums[right], nums[curr] = nums[curr], nums[right]
+                right -= 1
+            else:
+                curr += 1
